f07a1b747685cd5c104e4808e8f1057ad72f5721
Nicolás Bonardi <122838094+nicobonardi@users.noreply.github.com>
2024-11-10 17:57:38 -0300

    Actividad 2

--- Actividad 2/Act2.cpp
+++ Actividad 2/Act2.cpp
@@ -1,39 +1,58 @@
-//////Bibliotecas////// 
-#include <SFML/Window.hpp>  
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <iostream>
+#include <vector>
 using namespace sf;
-//////Variables//////
-Texture texture1;
-Sprite sprite1, sprite2, sprite3, sprite4;
-int main() {
-	//Cargamos la textura del archivo    
-	texture1.loadFromFile("rcircle.png");
-	// //Cargamos el material del sprite    
-	sprite1.setTexture(texture1);
-	sprite2.setTexture(texture1);
-	sprite3.setTexture(texture1);
-	sprite4.setTexture(texture1);
-	//Movemos el sprite1
-	sprite1.setPosition(0, 0);
-	//Movemos el sprite2
-	sprite2.setPosition(0, 472);
-	//Movemos el sprite3
-	sprite3.setPosition(672, 0);
-	//Movemos el sprite4
-	sprite4.setPosition(672, 472);
-	//Creamos la ventana     
-	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "Que ventana horrible");
-	// Loop principal      
-	while (App.isOpen()) {
-		// Limpiamos la ventana          
+using namespace std;
+int main()
+{
+	Texture redc;
+	Sprite sprite1, sprite2, sprite3, sprite4;
+	vector<Sprite> sprites;
+	redc.loadFromFile("rcircle.png");
+	sprite1.setTexture(redc); sprites.push_back(sprite1);
+	sprite2.setTexture(redc); sprite2.setPosition(672, 0); sprites.push_back(sprite2);
+	sprite3.setTexture(redc); sprite3.setPosition(672, 472); sprites.push_back(sprite3);
+	sprite4.setTexture(redc); sprite4.setPosition(0, 472); sprites.push_back(sprite4);
+
+	bool arrastrando = false;
+	Sprite* SpriteSeleccionado = nullptr;
+
+	sf::RenderWindow App(VideoMode(800, 600, 32), "Que ventana horrible");
+	while (App.isOpen())
+	{
+		Event event;
+		while (App.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				App.close();
+		}
+
+		if (event.type == sf::Event::MouseButtonPressed) {
+			if (event.mouseButton.button == sf::Mouse::Left) {
+				for (auto& sprite : sprites) {
+					if (sprite.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+						arrastrando = true;
+						SpriteSeleccionado = &sprite;
+						break;
+					}
+				}
+			}
+		}
+
+		if (event.type == sf::Event::MouseButtonReleased) {
+			if (event.mouseButton.button == sf::Mouse::Left) {
+				arrastrando = false;
+				SpriteSeleccionado = nullptr;
+			}
+		}
+
+		if (arrastrando && SpriteSeleccionado != nullptr) {
+			sf::Vector2i mousePosition = sf::Mouse::getPosition(App);
+			SpriteSeleccionado->setPosition(mousePosition.x - SpriteSeleccionado->getGlobalBounds().width / 2, mousePosition.y - SpriteSeleccionado->getGlobalBounds().height / 2);
+		}
 		App.clear();
-		// Dibujamos la escena          
-		App.draw(sprite1);
-		App.draw(sprite2);
-		App.draw(sprite3);
-		App.draw(sprite4);
-		// Mostramos la ventana          
+		for (const auto& sprite : sprites) {
+			App.draw(sprite);
+		}
 		App.display();
 	}
 	return 0;
